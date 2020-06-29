85eb0cbd687aec645acafe9cd785e61753b7b3ad
dongmin <ehdals9412@gmail.com>
2020-06-29 23:47:34 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -29,6 +29,13 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+// 스크롤 내릴수록 home 투명도 내리기
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
