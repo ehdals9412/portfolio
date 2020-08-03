5b71e1ea1f9502e9aab8417a036714054c2aa53c
dongmin <ehdals9412@gmail.com>
2020-08-03 14:28:10 +0900

    project update🎉

--- main.js
+++ main.js
@@ -1,73 +1,73 @@
-"use strict";
+'use strict';
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector("#navbar");
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark");
+    navbar.classList.remove('navbar--dark');
   }
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.id;
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove("open");
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
 
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
-navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
-const homeContactBtn = document.querySelector(".home__contact");
-homeContactBtn.addEventListener("click", () => {
-  scrollIntoView("#contact");
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
 });
 
 // 스크롤 내릴수록 home 투명도 내리기
-const home = document.querySelector(".home__container");
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
 
 // Make upArrow Button!
-const arrowUp = document.querySelector(".arrow-up");
-document.addEventListener("scroll", () => {
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add("visible");
+    arrowUp.classList.add('visible');
   } else {
-    arrowUp.classList.remove("visible");
+    arrowUp.classList.remove('visible');
   }
 });
 
-arrowUp.addEventListener("click", () => {
-  scrollIntoView("#home");
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
 
 // Projects!!
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
 
-workBtnContainer.addEventListener("click", (e) => {
+workBtnContainer.addEventListener('click', (e) => {
   // target에 값이 없으면 부모 요소의 filter 값을 가져와서 사용하겠다!
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
@@ -75,24 +75,24 @@ workBtnContainer.addEventListener("click", (e) => {
   }
 
   // Remove selection previous
-  const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
   const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  target.classList.add("selected");
+    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
 
   // filter값이 null이 아니면 anime-out class를 추가(투명도를 주고 밑으로 내림
   // 그 다음 300ms 이후에 setTimeout안에 있는 코드가 실행되는 거임
   // 쉽게 말하면 anime-out이 추가되고 300ms 이후에 함수안의 내용들이 실행됨
-  projectContainer.classList.add("anime-out");
+  projectContainer.classList.add('anime-out');
   setTimeout(() => {
     projects.forEach((project) => {
-      if (filter === "*" || filter === project.dataset.type) {
-        project.classList.remove("invisible");
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
       } else {
-        project.classList.add("invisible");
+        project.classList.add('invisible');
       }
     });
-    projectContainer.classList.remove("anime-out");
+    projectContainer.classList.remove('anime-out');
   }, 300);
 });
